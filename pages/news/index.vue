669f5c8ba11648b93e00ad2ec2889ee7b36109e4
Chris Clower <chris@fostercommerce.com>
2023-04-15 01:45:08 -0400

    API test

--- pages/news/index.vue
+++ pages/news/index.vue
@@ -1,5 +1,6 @@
 <script setup>
-  const { data: products } = await useFetch('https://fakestoreapi.com/products');
+  const { data: products } = await useFetch('https://news.clowerweb.com/');
+  console.log(products);
 
   useHead({
     title: 'News | Itsy News',
@@ -7,18 +8,18 @@
       { hid: 'description', name: 'description', content: 'News' },
       { hid: 'og:title', property: 'og:title', content: 'News' },
       { hid: 'og:description', property: 'og:description', content: 'News' },
-      { hid: 'og:image', property: 'og:image', content: 'https://fakestoreapi.com/img/81fPKd-2AYL._AC_SL1500_.jpg' },
-      { hid: 'og:url', property: 'og:url', content: 'https://fakestoreapi.com/products/1' },
+      { hid: 'og:image', property: 'og:image', content: '' },
+      { hid: 'og:url', property: 'og:url', content: '' },
       { hid: 'twitter:title', name: 'twitter:title', content: 'News' },
       { hid: 'twitter:description', name: 'twitter:description', content: 'News' },
-      { hid: 'twitter:image', name: 'twitter:image', content: 'https://fakestoreapi.com/img/81fPKd-2AYL._AC_SL1500_.jpg' },
+      { hid: 'twitter:image', name: 'twitter:image', content: '' },
       { hid: 'twitter:card', name: 'twitter:card', content: 'summary_large_image' },
     ]
   })
 </script>
 
 <template>
-  <h2>News index</h2>
+  <h2>News</h2>
 
   <div class="grid grid-cols-4 gap-5">
     <div v-for="p in products">
