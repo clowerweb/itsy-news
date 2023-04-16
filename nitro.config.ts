be173bd198b93aba7b5e02273572b3d025a3f07a
Chris Clower <chris@fostercommerce.com>
2023-04-16 00:14:03 -0400

    Update Nitro config

--- nitro.config.ts
+++ nitro.config.ts
@@ -1,6 +1,7 @@
 import { defineNitroConfig } from "nitropack";
 
 export default defineNitroConfig({
+	preset: 'netlify-builder',
 	routeRules: {
 		'/': { static: true },
 		'/about': { static: true },
